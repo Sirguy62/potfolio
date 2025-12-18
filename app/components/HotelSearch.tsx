f1aacea27a287b15821029e6ad8272a979585aed
Sirguy62 <gospele62@gmail.com>
2025-12-18 23:11:35 +0100

    Upgrade hotel

--- app/components/HotelSearch.tsx
+++ app/components/HotelSearch.tsx
@@ -74,37 +74,50 @@ export default function HotelSearch() {
 
   return (
     <div className="space-y-4 relative">
-      {/* SEARCH CONTROLS */}
-      <div className="flex flex-col md:flex-row w-full gap-4 bg-white p-4 rounded-lg">
-        <input
-          className="p-2 w-full text-gray-600 border rounded"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Enter city (e.g. Lagos)"
-        />
+      <div className=" bg-white p-4 rounded-lg flex flex-col gap-4 lg:flex-row w-full">
+        <div className="flex flex-col md:flex-row w-full gap-4">
+          <div className="w-full flex flex-col">
+            <p className="font-semibold text-gray-600 text-lg">City</p>
+            <input
+              className="p-2 w-full text-gray-600 border rounded"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              placeholder="Enter city (e.g. Lagos)"
+            />
+          </div>
 
-        {/* ✅ CHECK-IN */}
-        <input
-          type="date"
-          value={checkinDate}
-          onChange={(e) => setCheckinDate(e.target.value)}
-          className="p-2 text-gray-600 border rounded"
-        />
+          <div className="w-full flex flex-col">
+            <p className="font-semibold text-gray-600 text-lg">Checkin</p>
+            <input
+              type="date"
+              value={checkinDate}
+              aria-label="checkin"
+              onChange={(e) => setCheckinDate(e.target.value)}
+              className="p-2 text-gray-600 border rounded"
+            />
+          </div>
 
-        {/* ✅ CHECK-OUT */}
-        <input
-          type="date"
-          value={checkoutDate}
-          onChange={(e) => setCheckoutDate(e.target.value)}
-          className="p-2 text-gray-600 border rounded"
-        />
-
-        <button
-          onClick={handleSearch}
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:cursor-pointer"
-        >
-          Search
-        </button>
+          {/* ✅ CHECK-OUT */}
+          <div className="w-full flex flex-col">
+            <p className="font-semibold text-gray-600 text-lg">Checkout</p>
+            <input
+              type="date"
+              value={checkoutDate}
+              aria-label="checkout"
+              onChange={(e) => setCheckoutDate(e.target.value)}
+              className="p-2 text-gray-600 border rounded"
+            />
+          </div>
+        </div>
+        <div className=" flex flex-col mt-auto w-full lg:w-auto">
+          <p className="font-semibold text-gray-600 text-lg"></p>
+          <button
+            onClick={handleSearch}
+            className="bg-blue-600 text-white px-4 py-2 rounded hover:cursor-pointer"
+          >
+            Search
+          </button>
+        </div>
       </div>
 
       {loading && <Spinner />}
